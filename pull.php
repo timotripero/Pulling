c5c61f20e40a8e67249bc5c9cb96901d622c4376
timotripero <85303820+timotripero@users.noreply.github.com>
2021-06-03 13:45:04 -0300

    Actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,12 @@
+  <?Php
+  $Persona =  [
+              "nombre" => "Pedro",
+              "apellido" => "Sanchez",
+              "edad" => 50
+              ];
+              
+              
+              foreach ($persona as $valor) {
+                   echo "$valor <br>";
+               }
+  ?>             
